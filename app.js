2dd36f159ca7cc4f70948b7f8ce3dc44df22f239
Jon Hajrizi <jhajrizi@Jons-MacBook-Pro.local>
2018-11-22 17:43:24 -0500

    add error handling

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+const morgan = require('morgan');
 
 const eventRoutes = require('./api/routes/events');
 
+app.use(morgan('dev'));
+app.use(helmet());
+
 app.use('/events', eventRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
